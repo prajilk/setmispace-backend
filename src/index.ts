aef4a97cb5dc3ce74bc8db16ab9845112de766ec
Prajil K <prajilk20017@gmail.com>
2024-03-17 02:58:29 +0530

    changed port to 3000

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 import { adminRouter } from "./routes";
 import { businessRouter } from "./routes";
