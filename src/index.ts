caf8e4be9726aa2517afe5979693515e6188dbd6
Prajil K <prajilk20017@gmail.com>
2024-03-17 15:19:00 +0530

    updated vercel.json

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 import { adminRouter } from "./routes";
 import { businessRouter } from "./routes";
@@ -19,7 +19,12 @@ app.use(express.json({ limit: "200mb" }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://localhost:5173"],
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:5173",
+            "https://setmispace.vercel.app",
+            "https://admin-setmispace.vercel.app",
+        ],
         credentials: true,
     })
 );
